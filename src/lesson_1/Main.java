5d01e571e033c8025dfdd8364a3fb3388f5ea731
Mazura <dushacaterina@gmail.com>
2022-03-27 15:16:21 +0800

    Level2Lesson1

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -1,7 +1,25 @@
 package lesson_1;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+        Team man1 = new Team("Hank", 34, "black");
+        Team man2 = new Team("Sten", 29, "white");
+        Team man3 = new Team("Tom", 28, "red");
+        Team man4 = new Team("Albert", 30, "yellow");
+
+        Team[] mans = {man1, man2, man3, man4};
+
+        Course c = new Course(500, 6);
+
+        man1.teamInfo();
+        c.dolt(mans);
+        man2.teamInfo();
+        c.dolt(mans);
+        man3.teamInfo();
+        c.dolt(mans);
+        man4.teamInfo();
+        c.dolt(mans);
+
     }
 }
--- src/lesson_1/Team.java
+++ src/lesson_1/Team.java
@@ -1,4 +1,19 @@
 package lesson_1;
 
-public class Team {
+public class Team extends Main {
+    private String name;
+    private int age;
+    private String color;
+
+    public Team(String name, int age, String color){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void teamInfo(){
+        System.out.println(" Имя: " + name + " Возраст: " + age + " Цвет формы участника: " + color);
+
+    }
+
 }
